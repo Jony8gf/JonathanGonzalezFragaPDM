ed8b8ba7c98249b948ebcd05584b7c6403d84550
Jonathan <jgonzalezf09@educantabria.es>
2020-11-14 11:10:02 +0100

    Creacion de Registro de usuarios con Firebase Authentication

--- app/src/main/java/com/example/jonathangonzalezfragapdm/MainActivity2_Registro.java
+++ app/src/main/java/com/example/jonathangonzalezfragapdm/MainActivity2_Registro.java
@@ -8,18 +8,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2_Registro extends AppCompatActivity {
 
+    private FirebaseAnalytics mFirebaseAnalytics;
     private Button bt_continuar;
+    private EditText et_nombre, et_passwd1, et_passwd2, et_email;
+    private CheckBox chk_politicas;
+    private String nombre, passwd1, passwd2, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2__registro);
 
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
         //Asignacion de Button
         bt_continuar = findViewById(R.id.button_Continuar);
+        //Asignacion de EditTexts
+        et_nombre = findViewById(R.id.editText_Nombre);
+        et_passwd1 = findViewById(R.id.editText_Contraseña1);
+        et_passwd2 = findViewById(R.id.editText_Contraseña2);
+        et_email = findViewById(R.id.editText_Email);
+        //Asignacion de CheckBox
+        chk_politicas = findViewById(R.id.checkBox_privacidad);
+
+
     }
 
 
@@ -45,12 +69,39 @@ public class MainActivity2_Registro extends AppCompatActivity {
     }
 
 
+    public void RegistroDeUsuario(View view){
 
-    public void pasarActivitySobreMi(View view){
-        Intent intent = new Intent(this, MainActivity2_DatosPersonales.class);
-        startActivity(intent);
-        finish();
+        if(chk_politicas.isChecked()){
+
+            nombre = et_nombre.getText().toString();
+            passwd1 = et_passwd1.getText().toString();
+            passwd2 = et_passwd2.getText().toString();
+            email = et_email.getText().toString();
+
+            if(passwd1.equals(passwd2)){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, passwd1);
+                Intent intent = new Intent(this, MainActivity2_DatosPersonales.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(this, "Registrado", Toast.LENGTH_LONG).show();
+
+
+            }else{
+
+                et_passwd1.setText("");
+                et_passwd2.setText("");
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show();
+
+            }
+
+        }else{
+
+            Toast.makeText(this, "Debes aceptar las politicas de uso", Toast.LENGTH_LONG).show();
+
+        }
     }
+
 
     @Override
     protected void onStart() {
