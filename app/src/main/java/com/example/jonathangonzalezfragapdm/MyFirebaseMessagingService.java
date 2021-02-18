7a91a3da63ea9f5f643f07f81b159a48b322a4b7
Jonathan <jgonzalezf09@educantabria.es>
2021-02-18 12:09:01 +0100

    Arreglo de restrincion de com.example

--- app/src/main/java/com/example/jonathangonzalezfragapdm/MyFirebaseMessagingService.java
+++
@@ -1,6 +0,0 @@
-package com.example.jonathangonzalezfragapdm;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-}
